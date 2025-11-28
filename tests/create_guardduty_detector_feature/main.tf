abb1668ed7849a19439a3288932d3058b2080106
Keith Kissal <keith.kissal@plus3it.com>
2025-11-28 11:24:16 -0500

    adding description

--- tests/create_guardduty_detector_feature/main.tf
+++ tests/create_guardduty_detector_feature/main.tf
@@ -1,9 +1,6 @@
 # Creates a GuardDuty Malware Protection for this account.
-# - Creates a GuardDuty detector for this account
-# - Creates a GuardDuty Malware Protection for this account
-# - Creates IAM Role for GuardDuty Malware Protection for this account.
-# - Creates IAM Policy and Attachment for GuardDuty Malware Protection for this account.
-# - Creates S3 Bucket for GuardDuty Malware Protection for this account.
+# - Creates a GuardDuty detector for this account.
+# - Creates a GuardDuty detector feature for this account.
 
 module "malware_protection_plan" {
   source = "../../"
